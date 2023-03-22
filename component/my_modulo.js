9d27194d1ff6624532942038bd5a28d54642e5fd
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-03-21 23:52:17 -0500

    feat: :sparkles: complementos
    se le agrego algunas funcionalidades

--- component/my_modulo.js
+++ component/my_modulo.js
@@ -4,7 +4,31 @@ export  default{
         return{
             mensaje: "Gracias por la tortura :3",
             objeto: false,
+            button: null,
+            objetos:[],
+            nuevoobj:'',
+            Viearray:" ",
+            nuevoarray: ''
+        }
+    },
+    methods:{
+        nuevoobjt(){
+            this.objetos.push({
+                dato: this.nuevoobj
+            })
+            console.log(this.objetos)
+        },
+        almacenarray(){
+
+        },
+        operacionesarray(){
+
+
+        },
+        operacionescomb(){
+            
         }
     }
+    
 
 }
